35f1a38c5e2d17cab7f73f6580e47f3e30031188
Michael Baker <cleverbaker@gmail.com>
2025-01-21 12:28:26 -0800

    Complete main.js running demo. And some code cleanup.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,58 @@
-
-
 const {Engine, Body, Bodies, World, Composite, Render, Runner} = Matter;
 
 let engine;
+let world;
 let render;
+let runner;
+let box;
+let circle1;
 
-function setuo() {
-    noCanvas();
-    engine = Engine.create();
-    render = Render.create({
+// const canvasEl = document.getElementById('world')
+
+function setup() {
+   // noCanvas();
+    engine = Matter.Engine.create();
+    world = engine.world;
+    render = Matter.Render.create({
         element: document.body,
-        engine: engine
+        engine: engine,
+        options: {
+            width: 600,
+            height: 600,
+            outline: true,
+            wireframes: false,
+            background: 'transparent',
+            showAngleIndicator: false,
+            // canvas: canvasEl,
+        },
     });
+
+
+    box = Bodies.rectangle(100, 100, 80, 80);
+
+    Composite.add(engine.world, box);
+
+    circle1 = Bodies.circle(220, 100, 75, {
+        density: 0.042,
+        friction: 0.1,
+        frictionAir: 0.00001,
+        restitution: 0.8,
+        render: {
+            fillStyle: 'red',
+            strokeStyle: 'black',
+            lineWidth: 1
+        }
+    });
+
+    Composite.add(engine.world, circle1);
+
+
+    Render.run(render);
+
+    runner = Runner.create();
+    Runner.run(runner, engine);
 }
 
+window.addEventListener('load', function() {
+    setup();
+});
